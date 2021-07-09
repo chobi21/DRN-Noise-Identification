250822ab5eb89e1c449c69fbb1c9241feab8d0a9
chobi21 <81823418+chobi21@users.noreply.github.com>
2021-07-09 03:02:26 -0500

    Update classifier.py

--- Code/classifier.py
+++ Code/classifier.py
@@ -5,12 +5,17 @@
 
 
 import numpy as np
+import matplotlib.pyplot as plt
 import pandas as pd
+from numpy import *
 from sklearn.model_selection import train_test_split
+from sklearn import svm
 from sklearn import metrics
 from sklearn.metrics import plot_confusion_matrix
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.naive_bayes import GaussianNB
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
 
 dataset=pd.read_csv("/pathname/Dataset Name.csv", sep=',',header=None)
 dataset = dataset.dropna(axis=0, how='any', thresh=None, subset=None, inplace=False)
@@ -58,3 +63,43 @@ msClsListNB=[]
 for p in range(mTupELenNB):
     ap=misclassified_nb[-1][p]
     msClsListNB.append(ap)
+Knn = KNeighborsClassifier(n_neighbors=3)
+Knn.fit(X_train, y_train)
+y_pred_Knn=Knn.predict(X_test)
+print(" KNN Accuracy:",metrics.accuracy_score(y_test, y_pred_Knn))
+
+misclassified_Knn = np.where(y_test != y_pred_Knn)
+mTupELenKnn=len(misclassified_Knn[-1])
+msClsListKnn=[]
+
+for q in range(mTupELenKnn):
+    apq=misclassified_Knn[-1][q]
+    msClsListKnn.append(apq)
+    
+
+RF = RandomForestClassifier(max_depth=2, random_state=0)
+RF.fit(X_train, y_train)
+y_pred_RF=RF.predict(X_test)
+print(" RF Accuracy:",metrics.accuracy_score(y_test, y_pred_RF))
+
+misclassified_RF = np.where(y_test != y_pred_RF)
+mTupELenRF=len(misclassified_RF[-1])
+msClsListRF=[]
+
+for v in range(mTupELenRF):
+    apqv=misclassified_RF[-1][v]
+    msClsListRF.append(apqv)
+'''''
+
+SV=svm.SVC()
+SV.fit(X_train, y_train)
+y_pred_SV=SV.predict(X_test)
+print(" SVM Accuracy:",metrics.accuracy_score(y_test, y_pred_SV))
+
+misclassified_SV = np.where(y_test != y_pred_SV)
+mTupELenSV=len(misclassified_SV[-1])
+msClsListSV=[]
+
+for w in range(mTupELenSV):
+    apqvw=misclassified_SV[-1][w]
+    msClsListSV.append(apqvw)
